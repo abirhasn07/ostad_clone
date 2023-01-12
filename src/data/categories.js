deede9b6e255d58b67aff4fccce0298377de8f5b
abirhasn07 <iamabirhasan07@gmail.com>
2023-01-12 07:02:23 -0800

    update

---
+++ src/data/categories.js
@@ -0,0 +1,23 @@
+const categoriesData =[
+    {
+        img:"/cat-1.png",
+        name:"web developement",
+        course:"3",
+        workshop:"11"
+    },
+    {
+        img:"/cat-2.png",
+        name:"softwear/app development",
+        course:"5",
+        workshop:"9"
+    },
+    {
+        img:"/cat-3.png",
+        name:"design/illuttration",
+        course:"19",
+        workshop:"14"
+    },
+    
+]
+
+export default categoriesData
